529028ca00561dc58603f2b1a1d70e356b34683e
sjain0152 <sjain_be23@thapar.edu>
2025-06-29 12:42:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+// === checks datatype
+
+console.log("2"===2);
