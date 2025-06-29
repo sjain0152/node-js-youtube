194ebc17368115e1d6ccc687040722d2559fa95f
sjain0152 <sjain_be23@thapar.edu>
2025-06-29 12:27:35 +0000

    conversionoperations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -31,3 +31,36 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+// *********************** Operations *****************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " sahil"
+
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+2+2+3);
+console.log(1+2+"2");
+
+console.log(true);
+console.log(+true);
+console.log("");
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
